355ea002a3e76f0a17c4fae51233970f51dcdb45
Vultur Ana <vulturana1@gmail.com>
2022-03-25 15:28:59 +0200

    Initial Commit

---
+++ eScooter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eScooter
+//
+//  Created by Ana Vultur on 25.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eScooter/eScooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  eScooterApp.swift
+//  eScooter
+//
+//  Created by Ana Vultur on 25.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct eScooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
