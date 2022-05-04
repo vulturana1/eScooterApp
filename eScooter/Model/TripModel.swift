35a8ca362b498a87cb254bcb87d8321a78b591c7
Vultur Ana <vulturana1@gmail.com>
2022-05-04 14:57:44 +0300

    changed trip model

--- eScooter/Model/TripModel.swift
+++ eScooter/Model/TripModel.swift
@@ -10,29 +10,17 @@ import CoreLocation
 
 struct Trip: Codable {
     let id: String
-//    let username: String
-//    let scooterId: Int
     let coordinatesArray: [Coordinates]
     let totalTime: Int
     let distance: Int
     let cost: Double
-//    let status: String
-//    let startTime: String
-//    let endTime: String
-//    let v: Int
     
     enum CodingKeys: String, CodingKey {
         case id = "_id"
-//        case username = "username"
-//        case scooterId = "scooterId"
         case coordinatesArray = "coordinatesArray"
         case distance = "distance"
         case totalTime = "totalTime"
         case cost = "cost"
-//        case status = "status"
-//        case startTime = "startTime"
-//        case endTime = "endTime"
-//        case v = "__v"
     }
 }
 
